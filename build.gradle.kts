7d247bb267fa80918417a1180a2f4c2f984abf29
Alexey Sosnoviy <int-it@yandex.ru>
2020-10-10 17:21:34 +0300

    Обновлена версия SDBL парсера

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     api("org.eclipse.lsp4j", "org.eclipse.lsp4j", "0.9.0")
 
     // 1c-syntax
-    api("com.github.1c-syntax", "bsl-parser", "8e29bfd87c2a573cba03629f90592e69bab03596") {
+    api("com.github.1c-syntax", "bsl-parser", "e088928ec") {
         exclude("com.tunnelvisionlabs", "antlr4-annotations")
         exclude("com.ibm.icu", "*")
         exclude("org.antlr", "ST4")
